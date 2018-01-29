1c6dc57e4f4186dfb45d002e3f8952e6bbbcc105
fortymiles <mqgao@outlook.com>
2018-01-29 20:37:56 +0800

    split create train and test data to another file

--- fasttext_bn/classification_as_fasttext_bn.py
+++ fasttext_bn/classification_as_fasttext_bn.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
-from keras.preprocessing import sequence
 from keras.models import Model, Input
 from keras.layers import Dense, SpatialDropout1D, Dropout
 from keras.layers import Embedding, GlobalMaxPool1D, BatchNormalization
-from keras.preprocessing.text import Tokenizer
 import os
 import argparse
+from fasttext_bn.initial_train_test_data import get_train_test
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '4, 5, 6'
 
@@ -20,32 +19,13 @@ def main():
 
     args = parser.parse_args()
 
-    train_df = pd.read_csv(args.train_file_path)
-    test_df = pd.read_csv(args.test_file_path)
-
-    __NAN__ = '__NAN__'
-
-    X_train = train_df['comment_text'].fillna(__NAN__).values
-    y_train = train_df[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].values
-    X_test = test_df['comment_text'].fillna(__NAN__).values
-
     max_features = 50000
     maxlen = 150
     batch_size = 32
     embedding_dim = 64
     epochs = 10
 
-    tok = Tokenizer(num_words=max_features)
-    tok.fit_on_texts(list(X_train) + list(X_test))
-
-    x_train = tok.texts_to_sequences(X_train)
-    x_test = tok.texts_to_sequences(X_test)
-
-    print('average sequence length is {} '.format(np.mean(list(map(len, x_train)), dtype=int)))
-    print('average sequence length is {}'.format(np.mean(list(map(len, x_test)))))
-
-    X_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-    X_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+    X_train, y_train, X_test = get_train_test(args.train_file_path, args.test_file_path, max_features, maxlen)
 
     comment_input = Input(shape=(maxlen, ))
     comment_embedding = Embedding(max_features, embedding_dim, input_length=maxlen)(comment_input)
