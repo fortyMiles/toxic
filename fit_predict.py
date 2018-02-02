7f6f89aae39f8c74699bde765d66fddd4d41d16b
fortymiles <mqgao@outlook.com>
2018-02-03 01:15:14 +0800

    add not test mode

--- fit_predict.py
+++ fit_predict.py
@@ -62,6 +62,11 @@ def main():
         sub_train_indices = np.random.choice(range(X_size), size=int(X_size*0.1), replace=False)
         X_test_size = X_test.shape[0]
         sub_test_indices = np.random.choice(range(X_test_size), size=int(X_test_size*0.1), replace=False)
+    else:
+        X_size = X_train.shape[0]
+        sub_train_indices = np.random.choice(range(X_size), size=int(X_size*1), replace=False)
+        X_test_size = X_test.shape[0]
+        sub_test_indices = np.random.choice(range(X_test_size), size=int(X_test_size*1), replace=False)
 
         X_train = X_train[sub_train_indices]
         y_train = y_train[sub_train_indices]
