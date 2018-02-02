f4d5aeca54425295f6a2caa9f6e5f5f1bdd9880d
fortymiles <mqgao@outlook.com>
2018-02-02 18:21:29 +0800

    change fold count to mannual

--- toxic/train_utils.py
+++ toxic/train_utils.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import StratifiedKFold
+import numpy as np
 
 
 def _train_model(model, epoch, batch_size, train_x, train_y, val_x, val_y):
@@ -33,16 +33,30 @@ def _train_model(model, epoch, batch_size, train_x, train_y, val_x, val_y):
 
 
 def train_folds(X, y, epoch, fold_count, batch_size, get_model_func):
-    skf = StratifiedKFold(n_splits=fold_count, shuffle=False)
+    # skf = StratifiedKFold(n_splits=fold_count, shuffle=False)
     # skf = StratifiedKFold(y, n_folds=fold_count, shuffle=False)
+
+    fold_size = len(X) // fold_count
 
     models = []
     scores = []
-    for i, (train_indices, test_indices) in enumerate(skf.split(X, y)):
+    for i in range(fold_count):
+        fold_start = fold_size * i
+        fold_end = fold_size * (i+1)
+
+        if i == fold_count - 1:
+            fold_end = len(X)
+
+        train_x = np.concatenate([X[:fold_start], X[fold_end:]])
+        train_y = np.concatenate([y[:fold_start], y[fold_end:]])
+
+        val_x = X[fold_start:fold_end]
+        val_y = y[fold_start:fold_end]
+
         print('Running fold {}/{}'.format(i+1, fold_count))
         model = get_model_func()
 
-        model, score = _train_model(model, epoch, batch_size, X[train_indices], y[train_indices], X[test_indices], y[test_indices])
+        model, score = _train_model(model, epoch, batch_size, train_x, train_y, val_x, val_y)
 
         models.append(model)
         scores.append(score)
