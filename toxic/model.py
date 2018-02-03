2c53b98f57390776d134f5981e532fc543bd3b97
fortymiles <mqgao@outlook.com>
2018-02-03 09:54:16 +0800

    restore mode

--- toxic/model.py
+++ toxic/model.py
@@ -9,25 +9,16 @@ def get_model(embedding_matrix, sequence_length, dropout_rate, recurrent_units, 
     input_layer = Input(shape=(sequence_length,))
     embedding_layer = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1],
                                 weights=[embedding_matrix], trainable=True)(input_layer)
+
     x = Bidirectional(CuDNNGRU(recurrent_units, return_sequences=True))(embedding_layer)
     x = Dropout(dropout_rate)(x)
-
-    cells = [
-        GRUCell(recurrent_units),
-        GRUCell(recurrent_units),
-        GRUCell(recurrent_units),
-    ]
-
-    x = RNN(cells)(x)
-    # x = Bidirectional(CuDNNGRU(recurrent_units, return_sequences=False))(x)
-    # x = Dropout(dropout_rate)(x)
-    # x = Bidirectional(CuDNNGRU(recurrent_units, return_sequences=False))(x)
+    x = Bidirectional(CuDNNGRU(recurrent_units, return_sequences=False))(x)
     x = Dense(dense_size, activation="relu")(x)
     output_layer = Dense(6, activation="sigmoid")(x)
 
     model = Model(inputs=input_layer, outputs=output_layer)
     model.compile(loss='binary_crossentropy',
-                  optimizer=RMSprop(clipvalue=1, clipnorm=5, decay=1e-4),
+                  optimizer=RMSprop(clipvalue=1, clipnorm=1),
                   metrics=['accuracy'])
 
     return model
