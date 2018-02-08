08b0acab0d0ee72711329cf2a03736a8a4858f35
fortymiles <mqgao@outlook.com>
2018-02-08 17:07:27 +0800

    try add auc loss

---
+++ objectives.py
@@ -0,0 +1,46 @@
+from keras import backend as K
+
+_epsilon = K.epsilon()
+
+
+def roc_auc_score(y_pred, y_true):
+    """ ROC AUC Score.
+    Approximates the Area Under Curve score, using approximation based on
+    the Wilcoxon-Mann-Whitney U statistic.
+    Yan, L., Dodier, R., Mozer, M. C., & Wolniewicz, R. (2003).
+    Optimizing Classifier Performance via an Approximation to the Wilcoxon-Mann-Whitney Statistic.
+    Measures overall performance for a full range of threshold levels.
+    Arguments:
+        y_pred: `Tensor`. Predicted values.
+        y_true: `Tensor` . Targets (labels), a probability distribution.
+    """
+    pos = y_true[K.cast(y_true, bool)]
+    neg = y_pred[~K.cast(y_true, bool)]
+
+    pos = K.expand_dims(pos, 0)
+    neg = K.expand_dims(neg, 1)
+
+    gamma = 0.2
+    p = 3
+
+    difference = K.zeros_like(pos * neg) + pos - neg - gamma
+    masked = difference[difference < 0.0]
+    # with tf.name_scope("RocAucScore"):
+    #
+    #     pos = tf.boolean_mask(y_pred, tf.cast(y_true, tf.bool))
+    #     neg = tf.boolean_mask(y_pred, ~tf.cast(y_true, tf.bool))
+
+        # pos = tf.expand_dims(pos, 0)
+        # neg = tf.expand_dims(neg, 1)
+
+        # original paper suggests performance is robust to exact parameter choice
+        # gamma = 0.2
+        # p     = 3
+
+        # difference = tf.zeros_like(pos * neg) + pos - neg - gamma
+
+        # masked = tf.boolean_mask(difference, difference < 0.0)
+        #
+        # return tf.reduce_sum(tf.pow(-masked, p))
+
+    return K.sum(K.pow(-masked, p))
