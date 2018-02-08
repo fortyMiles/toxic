e061af53f2d7f9acf3f1a984345306a1f9c7e5d9
fortymiles <mqgao@outlook.com>
2018-02-08 18:33:11 +0800

    add tf

--- objectives.py
+++ objectives.py
@@ -7,7 +7,36 @@ from keras.layers import Lambda
 _epsilon = K.epsilon()
 
 
-def roc_auc_score(y_pred, y_true):
+def roc_auc_score_tf(y_pred, y_true):
+    """ ROC AUC Score.
+    Approximates the Area Under Curve score, using approximation based on
+    the Wilcoxon-Mann-Whitney U statistic.
+    Yan, L., Dodier, R., Mozer, M. C., & Wolniewicz, R. (2003).
+    Optimizing Classifier Performance via an Approximation to the Wilcoxon-Mann-Whitney Statistic.
+    Measures overall performance for a full range of threshold levels.
+    Arguments:
+        y_pred: `Tensor`. Predicted values.
+        y_true: `Tensor` . Targets (labels), a probability distribution.
+    """
+    with tf.name_scope("RocAucScore"):
+        pos = tf.boolean_mask(y_pred, tf.cast(y_true, tf.bool))
+        neg = tf.boolean_mask(y_pred, ~tf.cast(y_true, tf.bool))
+
+        pos = tf.expand_dims(pos, 0)
+        neg = tf.expand_dims(neg, 1)
+
+        # original paper suggests performance is robust to exact parameter choice
+        gamma = 0.2
+        p = 3
+
+        difference = tf.zeros_like(pos * neg) + pos - neg - gamma
+
+        masked = tf.boolean_mask(difference, difference < 0.0)
+
+        return tf.reduce_sum(tf.pow(-masked, p))
+
+
+def roc_auc_score_keras(y_pred, y_true):
     """ ROC AUC Score.
     Approximates the Area Under Curve score, using approximation based on
     the Wilcoxon-Mann-Whitney U statistic.
@@ -43,3 +72,6 @@ def roc_auc_score(y_pred, y_true):
         # masked = K.boolean_mask(difference, difference < 0.0)
 
         return K.sum(tf.pow(-masked, p))
+
+
+roc_auc_score = roc_auc_score_tf
