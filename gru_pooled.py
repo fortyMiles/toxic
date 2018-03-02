5a16645e2bed0fe18ea94cda24e7b932b50f7b2b
fortymiles <mqgao@outlook.com>
2018-03-02 15:09:09 +0800

    add encoding

--- gru_pooled.py
+++ gru_pooled.py
@@ -45,7 +45,7 @@ x_test = sequence.pad_sequences(X_test, maxlen=maxlen)
 def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
 
 
-embeddings_index = dict(get_coefs(*o.rstrip().rsplit(' ')) for o in open(EMBEDDING_FILE))
+embeddings_index = dict(get_coefs(*o.rstrip().rsplit(' ')) for o in open(EMBEDDING_FILE, encoding='utf-8'))
 
 word_index = tokenizer.word_index
 nb_words = min(max_features, len(word_index))
