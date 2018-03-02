7a28d35bc58b5f61946c838808da3868662d917d
fortymiles <mqgao@outlook.com>
2018-03-02 16:32:20 +0800

    change gru to cudaGru

--- gru_pooled.py
+++ gru_pooled.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
 from keras.models import Model
-from keras.layers import Input, Dense, Embedding, SpatialDropout1D, concatenate
+from keras.layers import Input, Dense, Embedding, SpatialDropout1D, concatenate, CuDNNGRU
 from keras.layers import GRU, Bidirectional, GlobalAveragePooling1D, GlobalMaxPooling1D
 from keras.preprocessing import text, sequence
 from keras.callbacks import Callback
@@ -80,7 +80,7 @@ def get_model():
     inp = Input(shape=(maxlen,))
     x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
     x = SpatialDropout1D(0.2)(x)
-    x = Bidirectional(GRU(80, return_sequences=True))(x)
+    x = Bidirectional(CuDNNGRU(80, return_sequences=True))(x)
     avg_pool = GlobalAveragePooling1D()(x)
     max_pool = GlobalMaxPooling1D()(x)
     conc = concatenate([avg_pool, max_pool])
