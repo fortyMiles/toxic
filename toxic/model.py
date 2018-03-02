43d1bd1a51539d8ea694c3d7ebda21e7a89b8897
fortymiles <mqgao@outlook.com>
2018-03-02 14:36:35 +0800

    add pooling

--- toxic/model.py
+++ toxic/model.py
@@ -3,7 +3,7 @@ from keras.layers import Bidirectional, Dropout, CuDNNGRU
 from keras.models import Model
 from keras.optimizers import RMSprop
 from objectives import roc_auc_score
-from keras.layers import GRUCell, RNN, LSTMCell
+from keras.layers import GRUCell, RNN, LSTMCell, GlobalAvgPool1D, GlobalMaxPool1D, concatenate
 from keras.losses import categorical_crossentropy
 
 
@@ -18,8 +18,11 @@ def get_model(embedding_matrix, sequence_length, dropout_rate, recurrent_units, 
     x = Dropout(dropout_rate)(x)
     x = Bidirectional(CuDNNGRU(recurrent_units, return_sequences=True))(x)
     x = Dropout(dropout_rate)(x)
-    x = Bidirectional(CuDNNGRU(recurrent_units, return_sequences=False))(x)
-    x = Dense(dense_size, activation="relu")(x)
+    avg_pool = GlobalAvgPool1D(x)
+    max_pool = GlobalMaxPool1D(x)
+    conc = concatenate([avg_pool, max_pool])
+    # x = Bidirectional(CuDNNGRU(recurrent_units, return_sequences=False))(x)
+    x = Dense(dense_size, activation="relu")(conc)
     output_layer = Dense(6, activation="sigmoid")(x)
 
     model = Model(inputs=input_layer, outputs=output_layer)
